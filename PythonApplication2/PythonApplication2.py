cfa6557ed2a3795c024a86318e24c25d0cfbb6c2
hanleyjaa123 <jacksonhanley15@gmail.com>
2023-09-09 04:24:26 -0400

    Removed random code for testing

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,11 +1,11 @@
 import math
 import data
-import random
+#import random
 
 
 
 
-#takes users number of problems correct and how many problems there were and will calcuate their score. 
+#takes users number of problems correct and how many problems there  was and will calcuate their score. 
 def gradecal(grade, total):
    multi = int(grade) / int(total) 
    awnsr = multi * 100
@@ -49,56 +49,4 @@ else:
 
 
 
-#wgrade = input("what is your desired grade?")
 
-
-#total = input("How many problems total?")
-
-#print(required_grade(total, wgrade))
-
-
-# Quick sort algorithm takes piviot point in a list, and sorts by creating a new list and one by 
-# one checks to see if the following number is larger or smaller than pivot point
-def quick_sort(sequence):
-    length = len(sequence)
-    if length <= 1:
-        return sequence
-    else:
-        pivot = sequence.pop()
-
-    items_greater = []
-    items_lower = []
-     
-    for item in sequence:
-        if item > pivot:
-            items_greater.append(item)
-
-        else:
-            items_lower.append(item)
-
-    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
-
-print(quick_sort(data.data_finder()))
-#print(type(data.data_finder()))
-#def random_gen(int):
-    
- #   randomlist = random.sample(range(1, 99), 50)
-  #  return randomlist
-
-#ammount_numbers = input("how many random numbers would you like?")
-#int(ammount_numbers)
-
-#print(random_gen(ammount_numbers))
-
-
-
-#def interfaces():
-   
-  #  list = {"john": 4, "ammy": 2}
-  #  return list[1]
-
-
-
-
-
-#print(interfaces)
